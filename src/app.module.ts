3d54a957ffb446a1d950d7f025c124e100dbbd46
Jocel <nardijocel@gmail.com>
2024-03-05 17:26:57 +0300

    reset project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
 
-import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
-import { TypeormModule } from './infrastructure/config/typeorm/typeorm.module';
-
 @Module({
-  imports: [EnvironmentConfigModule, TypeormModule],
+  imports: [],
 })
 export class AppModule {}
